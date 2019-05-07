004c0f1225d2acb0364f0d4aa26df45730fda0eb
larsslagboom <lslagboom@hotmail.nl>
2019-05-07 21:52:21 +0200

    Edited 2 files

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Network network = new Network("optimaal");
+        Optimize optimized = new Optimize(network,99.99);
 
     }
 }
