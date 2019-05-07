5f37dbb8abe3b5e27a130ca798d6d8aa04082ed4
larsslagboom <lslagboom@hotmail.nl>
2019-05-07 16:42:52 +0200

    edited the network class ready to write algorithm using this

--- src/Network.java
+++ src/Network.java
@@ -5,6 +5,32 @@ public class Network {
     private String name;
     private ArrayList<Component> network;
 
+    public Network(String name){
+        this.name = name;
+    }
 
+    public Network(String name, ArrayList<Component> network) {
+        this.name = name;
+        this.network = network;
+    }
 
+    public void addComponent(Component component){
+        network.add(component);
+    }
+
+    public double CalculatePrice(){
+        return 0.00;
+    }
+
+    public double CalculateUptime(){
+        return 0.00;
+    }
+
+    public ArrayList<Component> getNetwork(){
+        return this.network;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
