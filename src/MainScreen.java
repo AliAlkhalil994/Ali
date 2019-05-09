f1c468a2447dc0fd47107d3d95390a37f3109089
lars <lslagbo6338@student.landstede.nl>
2019-05-09 13:17:39 +0200

    edited main

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -2,9 +2,7 @@ import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class MainScreen extends JFrame implements ActionListener {
 
@@ -17,6 +15,9 @@ public class MainScreen extends JFrame implements ActionListener {
     private JMenu menuBerekening, menuOptimaliseer, menuOpslaan;
     private JMenuItem menuItem;
 
+    //items
+    private JLabel web1,web2,web3;
+
     public MainScreen(Network liveComponents) {
 
         //get current screen size
@@ -24,10 +25,10 @@ public class MainScreen extends JFrame implements ActionListener {
 
         //default config screen
         setTitle("Hoofdscherm");
-        setLayout(new GridLayout(3, 1));
+        setLayout(new GridLayout(2, 9));
         setSize(screenSize.width, screenSize.height);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setUndecorated(true);
+        //setUndecorated(true);
 
 
         //menu config
@@ -51,6 +52,50 @@ public class MainScreen extends JFrame implements ActionListener {
         menuBar.add(menuOpslaan);
 
         setJMenuBar(menuBar);
+
+        web1 = new JLabel();
+        web2 = new JLabel();
+        web3 = new JLabel();
+        web1.setIcon(new ImageIcon("D:\\Users\\lars\\Desktop\\50.png"));
+        web2.setIcon(new ImageIcon("D:\\Users\\lars\\Desktop\\50.png"));
+
+        MouseListener m1 = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) { }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+                JComponent jc = (JComponent) e.getSource();
+                TransferHandler th = jc.getTransferHandler();
+                th.exportAsDrag(jc, e, TransferHandler.COPY);
+
+                System.out.println("test");
+
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) { }
+
+            @Override
+            public void mouseEntered(MouseEvent e) { }
+
+            @Override
+            public void mouseExited(MouseEvent e) { }
+        };
+
+        web1.addMouseListener(m1);
+        web2.addMouseListener(m1);
+        web3.addMouseListener(m1);
+
+        web1.setTransferHandler(new TransferHandler("icon"));
+        web2.setTransferHandler(new TransferHandler("icon"));
+        web3.setTransferHandler(new TransferHandler("icon"));
+
+        add(web1);
+        add(web2);
+        add(web3);
 
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
