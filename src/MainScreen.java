6b3fd027e0ce1a5d856ac12af8ac4d834fa8bd64
lars <lslagbo6338@student.landstede.nl>
2019-05-09 11:38:34 +0200

    added mainscreen

---
+++ src/MainScreen.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainScreen extends JFrame implements ActionListener {
+
+    private Network liveComponents;
+    private double totalPrice;
+    private double uptime;
+
+    public MainScreen(Network liveComponents){
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
